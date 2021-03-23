86c93a082c3507801e1b2307699d0fe5997b9342
wonik <twer4774@gmail.com>
2021-03-23 10:53:51 +0900

    code : spring data

---
+++ Spring/SpringMaster5.0/Code/SpringData/src/main/java/com/code/SpringDataJpaFirstExapmpleApplication.java
@@ -0,0 +1,12 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaFirstExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaFirstExampleApplication.class, args);
+
+    }
+}
---
+++ Spring/SpringMaster5.0/Code/SpringData/src/main/java/com/code/repository/TodoRepository.java
@@ -0,0 +1,21 @@
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.in28minutes.model.Todo;
+
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+    List<Todo> findByTitleAndDescription(String title, String description);
+
+    List<Todo> findDistinctTodoByTitleOrDescription(String title,
+                                                    String description);
+
+    List<Todo> findByTitleIgnoreCase(String title);
+
+    List<Todo> findByTitleOrderByIdDesc(String title);
+
+    List<Todo> findByIsDoneTrue();
+
+}
